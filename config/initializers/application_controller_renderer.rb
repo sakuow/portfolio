142727caa81190603ab668cc89baf8dd49081d4e
sakuow <yusaku1335@gmail.com>
2021-11-22 07:43:25 +0000

    コメント

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'dfab32de914a156ebe927a300f8c49e76e09128298a35699826c3a50fecd1c3b844b4b27b3d0425eab066f8e54a7c395e868866b0fe614e8b8bf91e0d8cdbdec'
