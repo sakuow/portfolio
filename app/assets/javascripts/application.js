66b5a5d8b7a5393c9b2fd82cb3b695e265d0c2c2
sakuow <yusaku1335@gmail.com>
2021-11-25 11:36:36 +0000

    top/aboutページ 修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,14 +22,40 @@
 //= require activestorage
 
 /*global $*/
+
+// topのスライド
+$(document).on('turbolinks:load', function () {
+  $(".top-visual-slide").skippr({
+    // slideshowSpeed:1000,
+    // animation:"fade",
+    // controlNav:false,
+    // directionNav:false,
+    // pauseOnHover:false,
+    // reverse:false,
+    // animationSpeed:1500,
+    // sllideshow:true,
+    // childrenElementType : 'div'
+    transition:'fade',
+    navType:"bubble",
+    speed:1500,
+    arrows:false,
+    autoPlay:true,
+    childrenElementType:'div',
+    autoPlayDuration:5000,
+    hidePrevious:true
+  });
+});
+
+// _indexのスライド
 $(document).on('turbolinks:load', function() {
   $(".form").skippr({
     transition : 'slide',
-    speed : 1000,
-    arrows : false,
-    autoPlay : false,
-    childrenElementType : 'div',
-    autoPlayDuration : 3000,
-    hidePrevious : true
+    speed:1500,
+    arrows:false,
+    autoPlay:true,
+    navType:"bubble",
+    childrenElementType:'div',
+    autoPlayDuration:6000,
+    hidePrevious:true
   })
 });
