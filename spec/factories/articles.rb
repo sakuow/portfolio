e9c8f95222c0f9b2019bc29dafbb63c086cb167b
sakuow <yusaku1335@gmail.com>
2021-11-30 09:12:10 +0000

    rspec models

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :article do
     title { Faker::Lorem.characters(number:10) }
     body { Faker::Lorem.characters(number:30) }
+    # images_files { Faker::
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     introduction { Faker::Lorem.characters(number:20) }
+    # profile_image_id { Rack::Test::UploadedFile.new(file.join(Rails.root, 'public/images/.jpg')) }
     password { 'test12345' }
     password_confirmation { password }
   end
