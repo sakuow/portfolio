ae36dd9eb2c96702a2997681197779141a9fcffc
sakuow <yusaku1335@gmail.com>
2021-12-07 08:01:54 +0000

    homes/articles リーダブル修正

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -3,10 +3,10 @@ Refile.backends['store'] = Refile::Backend::FileSystem.new('public/uploads/')
 require 'refile/s3'
 
   aws = {
-    access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキーID
-    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], # シークレットアクセスキー
-    region: 'ap-northeast-1', # リージョン
-    bucket: 'portfoliomarks', # バケット名
+    access_key_id: ENV['S3_ACCESS_KEY_ID'],
+    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], 
+    region: 'ap-northeast-1',
+    bucket: 'portfoliomarks', 
   }
 
   Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
